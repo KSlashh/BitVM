6538cc3d8adb3e7a9e5b53e0a0729306cc98772b
KSlashh <48985735+KSlashh@users.noreply.github.com>
2025-04-22 10:16:17 +0800

    update testnet timelock

--- goat/src/utils.rs
+++ goat/src/utils.rs
@@ -4,7 +4,7 @@ use bitcode::{Decode, Encode};
 use bitcoin::Network;
 
 const NUM_BLOCKS_REGTEST: u32 = 1;
-const NUM_BLOCKS_TESTNET: u32 = 1;
+const NUM_BLOCKS_TESTNET: u32 = 7;
 
 pub fn num_blocks_per_network(network: Network, mainnet_num_blocks: u32) -> u32 {
     match network {
