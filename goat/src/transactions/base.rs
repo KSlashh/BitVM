5d92292fd85af636e6871785f427fdb27ce6dc25
KSlashh <48985735+KSlashh@users.noreply.github.com>
2025-04-28 12:14:51 +0800

    update disprove fee logic

--- goat/src/transactions/base.rs
+++ goat/src/transactions/base.rs
@@ -50,7 +50,7 @@ pub const MIN_RELAY_FEE_ASSERT_INITIAL: u64 = relay_fee(16380);
 pub const MIN_RELAY_FEE_ASSERT_COMMIT: u64 = relay_fee(98350);
 pub const MIN_RELAY_FEE_ASSERT_FINAL: u64 = relay_fee(490);
 pub const MIN_RELAY_FEE_CHALLENGE: u64 = relay_fee(317);
-pub const MIN_RELAY_FEE_DISPROVE: u64 = relay_fee(400000);
+pub const MIN_RELAY_FEE_DISPROVE: u64 = relay_fee(0); // let Challenger pay for disprove-tx
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Input {
